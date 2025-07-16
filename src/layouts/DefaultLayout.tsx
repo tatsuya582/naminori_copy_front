b406d0807a0932fe0f4f87a6a2d30d19d788d259
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2025-07-16 22:13:53 +0900

    add: ヘッダーを画面に追加

---
+++ src/layouts/DefaultLayout.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/header/Header";
+// import Footer from "@/components/Footer";
+
+const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">{children}</main>
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default DefaultLayout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import DefaultLayout from "@/layouts/DefaultLayout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <DefaultLayout>
+      <Component {...pageProps} />
+    </DefaultLayout>
+  );
 }
