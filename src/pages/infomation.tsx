19fa59328f93766c68a9a1ca4808d7f5654284cd
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2025-07-16 22:28:01 +0900

    add: フッター用のリンク先のページを仮作成

---
+++ src/pages/infomation.tsx
@@ -0,0 +1,7 @@
+export default function ArticlesPage() {
+  return (
+    <div>
+      <h1>会社概要</h1>
+    </div>
+  );
+}
---
+++ src/pages/terms.tsx
@@ -0,0 +1,7 @@
+export default function ArticlesPage() {
+  return (
+    <div>
+      <h1>利用規約</h1>
+    </div>
+  );
+}
