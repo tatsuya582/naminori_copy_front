638e59c59a4e380220ab7e1ab6603b7f510e6389
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2025-07-20 23:41:12 +0900

    add: デフォルトレイアウトを修正

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -6,7 +6,14 @@ const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow">{children}</main>
+      <main className="flex-1">
+        <div>
+          <div className="border-t border-b border-stone-200">
+            <div className="max-w-[1000px] w-full mx-auto"></div>
+          </div>
+          {children}
+        </div>
+      </main>
       <Footer />
     </div>
   );
