3ce06252964c6d009518e491ec8a369f0fd4a178
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2025-07-21 12:02:51 +0900

    add: トーストが使えるように設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import DefaultLayout from "@/layouts/DefaultLayout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <DefaultLayout>
       <Component {...pageProps} />
+      <Toaster richColors position="top-right" />
     </DefaultLayout>
   );
 }
