cd2fbc32bc0f66b86eea7b8ec24ce6c165c4847c
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2025-07-22 15:39:36 +0900

    add: ログイン状態で使うリンクのページを仮作成

---
+++ src/pages/user/applys.tsx
@@ -0,0 +1,7 @@
+export default function ApplysPage() {
+  return (
+    <div>
+      <h1>応募管理</h1>
+    </div>
+  );
+}
---
+++ src/pages/user/setting/privacy.tsx
@@ -0,0 +1,7 @@
+export default function PrivacyPage() {
+  return (
+    <div>
+      <h1>公開設定</h1>
+    </div>
+  );
+}
